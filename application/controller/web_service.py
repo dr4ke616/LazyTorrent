9b7d944314693398ed781071f8cc29e6c5ffc408
Adam Drakeford <adam.drakeford@dedsert.com>
2014-03-26 18:27:55 +0000

    Refactored web_service for new Movie model

--- application/controller/web_service.py
+++ application/controller/web_service.py
@@ -50,10 +50,10 @@ class WebService(controller.Controller):
             [
                 {
                     "title": string,
-                    "dvd_release": date object,
-                    "theater_release": date object,
-                    "rating": string,
-                    "download_now": boolean
+                    "dvd_release": date object,     (optional)
+                    "theater_release": date object, (optional)
+                    "rating": string,               (optional)
+                    "download_when": date object    (optional)
                 },
                 {
                     ...
@@ -61,8 +61,6 @@ class WebService(controller.Controller):
             ]
         """
 
-        ## TODO: This needs to be sent over application/json
-        # header request. Some reason it wouldnt work for me.
         data = kwargs.get('data')
         data = urllib.unquote(data).decode('utf8')
         log.msg('JSON: {}'.format(data))
@@ -79,36 +77,53 @@ class WebService(controller.Controller):
             if check is not None:
                 return Ok(check)
 
-        count = 0
         for movie in data:
-            download_now = movie.pop('download_now', True)
-            movie['name'] = movie.pop('title')
-            dvd_release = movie.get('dvd_release')
-            theater_release = movie.get('theater_release')
+            error = self._create_movie(movie)
 
-            if dvd_release is not None:
-                movie['dvd_release'] = parser.parse(dvd_release)
-
-            if theater_release is not None:
-                movie['theater_release'] = parser.parse(theater_release)
-
-            try:
-                count += 1
-                Movie(**movie).create(download_now=download_now)
-            except Exception as e:
-                msg = 'Error adding Movie {}'.format(movie['name'])
-                log.err('{}: {}'.format(msg, e))
-                [log.err(line) for line in
-                    traceback.format_exc().splitlines()]
+            if error is not None:
                 return InternalServerError(
-                    self._generate_response(code=500, message=msg))
+                    self._generate_response(code=500, message=error))
 
         return Ok(self._generate_response(
             code=0,
-            message='Added {} out of {} Movies to queue'.format(
-                count, len(data))
-            )
+            message='Added {} Movies to queue'.format(len(data)))
         )
+
+    def _create_movie(self, movie):
+        """ Creates a movie entry in DB.
+            :param movie: dict containing movie data
+        """
+
+        name = movie.get('title')
+        dvd_release = movie.get('dvd_release')
+        theater_release = movie.get('theater_release')
+        rating = movie.get('rating')
+        download_when = movie.get('download_when')
+
+        if dvd_release is not None:
+            dvd_release = parser.parse(dvd_release)
+
+        if theater_release is not None:
+            theater_release = parser.parse(theater_release)
+
+        if download_when is not None:
+            download_when = parser.parse(download_when)
+
+        args = {
+            'name': name,
+            'dvd_release': dvd_release,
+            'theater_release': theater_release,
+            'rating': rating
+        }
+
+        try:
+            Movie(**args).create(download_when=download_when)
+        except Exception as e:
+            msg = 'Error adding Movie {}'.format(movie['name'])
+            log.err('{}: {}'.format(msg, e))
+            [log.err(line) for line in
+                traceback.format_exc().splitlines()]
+            return msg
 
     @route('/add/tv_show', method='POST')
     def add_tv_show(self, request, **kwargs):
