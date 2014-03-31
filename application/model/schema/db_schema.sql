15ea74fab7662668d9675bcfb8525ee25fe9998e
Adam Drakeford <adamdrakeford@gmail.com>
2014-03-31 21:06:59 +0100

    Reverted to 1d5c909

--- application/model/schema/db_schema.sql
+++ application/model/schema/db_schema.sql
@@ -3,8 +3,6 @@ SET foreign_key_checks = 0;
 DROP TABLE IF EXISTS `torrent_queue`;
 DROP TABLE IF EXISTS `tv_shows`;
 DROP TABLE IF EXISTS `movies`;
-DROP TABLE IF EXISTS `bittorrent_clients`;
-DROP TABLE IF EXISTS `bittorrent_queue`;
 
 --
 -- Table structure for table `torrent_queue`
@@ -50,36 +48,6 @@ CREATE TABLE IF NOT EXISTS `movies` (
   `theater_release` datetime DEFAULT NULL,
   `rating` int(11) DEFAULT NULL,
   PRIMARY KEY (`movie_id`),
-  FOREIGN KEY (`torrent_queue_id`) REFERENCES torrent_queue(`torrent_queue_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
-
---
--- Table structure for table `bittorrent_clients`
---
-
-CREATE TABLE IF NOT EXISTS `bittorrent_clients` (
-  `client_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `description` varchar(256) NOT NULL,
-  PRIMARY KEY (`client_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
-INSERT INTO `bittorrent_clients` (`description`)
-VALUES ('Transmission');
-
---
--- Table structure for table `bittorrent_queue`
---
-
-CREATE TABLE IF NOT EXISTS `bittorrent_queue` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `client_id` int(11) unsigned NOT NULL COMMENT 'Id for the client type',
-  `torrent_id` int(11) unsigned NOT NULL COMMENT 'Id returned from the bittorent clinet',
-  `torrent_queue_id` int(11) unsigned NOT NULL COMMENT 'Foreign key to reference main torrent_queue table ',
-  `name` varchar(256) NOT NULL,
-  `url` text NOT NULL,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`client_id`) REFERENCES bittorrent_clients(`client_id`),
   FOREIGN KEY (`torrent_queue_id`) REFERENCES torrent_queue(`torrent_queue_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
--- application/test/test_models.py
+++ application/test/test_models.py
@@ -9,16 +9,13 @@
 .. modelauthor:: Adam Drakeford <adam.drakeford@gmail.com>
 """
 
-import os
-import unittest
-
 from datetime import datetime
 
+import unittest
 
 from ..model.torrent_queue import TorrentQueue
 from ..model.tv_show import TVShow
 from ..model.movie import Movie
-from application.model.bittorrent_queue import BittorrentQueue
 
 
 class ModelsTestCase(unittest.TestCase):
@@ -26,11 +23,14 @@ class ModelsTestCase(unittest.TestCase):
     def setUp(self):
         ## Tear down databse and create schema
         store = TorrentQueue.database.store()
-        sql = self._load_sql_schema()
-        store.execute(sql)
+        store.execute("SET FOREIGN_KEY_CHECKS=0;")
+        store.execute("TRUNCATE TABLE torrent_queue;")
+        store.execute("TRUNCATE TABLE movies;")
+        store.execute("TRUNCATE TABLE tv_shows;")
+        store.execute("SET FOREIGN_KEY_CHECKS=1;")
         store.commit()
 
-    def atest_torrent_queue_no_when(self):
+    def test_torrent_queue_no_when(self):
         queue = TorrentQueue()
         queue.media_type = u'MOVIE'
         queue.query = u'Titanic'
@@ -41,7 +41,7 @@ class ModelsTestCase(unittest.TestCase):
         queue = TorrentQueue.load()
         self.assertEquals(1, len(queue))
 
-    def atest_torrent_queue_with_when(self):
+    def test_torrent_queue_with_when(self):
         queue = TorrentQueue()
         queue.media_type = u'MOVIE'
         queue.query = u'Titanic'
@@ -53,7 +53,7 @@ class ModelsTestCase(unittest.TestCase):
         queue = TorrentQueue.load()
         self.assertEquals(1, len(queue))
 
-    def atest_movie_no_when(self):
+    def test_movie_no_when(self):
         movie = Movie()
         movie.name = u'Titanic'
         movie.dvd_release = datetime.strptime('1989-08-17', '%Y-%m-%d')
@@ -67,7 +67,7 @@ class ModelsTestCase(unittest.TestCase):
         movie = Movie.load(movie_id=1)
         self.assertEquals(1, len(movie))
 
-    def atest_movie_with_when(self):
+    def test_movie_with_when(self):
         movie = Movie()
         movie.name = u'Titanic'
         movie.dvd_release = datetime.strptime('1989-08-17', '%Y-%m-%d')
@@ -81,7 +81,7 @@ class ModelsTestCase(unittest.TestCase):
         movie = Movie.load(movie_id=1)
         self.assertEquals(1, len(movie))
 
-    def atest_movie_with_when_before_release_date(self):
+    def test_movie_with_when_before_release_date(self):
         movie = Movie()
         movie.name = u'Titanic'
         movie.dvd_release = datetime.strptime('2089-08-17', '%Y-%m-%d')
@@ -95,7 +95,7 @@ class ModelsTestCase(unittest.TestCase):
         movie = Movie.load(movie_id=1)
         self.assertEquals(1, len(movie))
 
-    def atest_tv_show_no_when(self):
+    def test_tv_show_no_when(self):
         tv_show = TVShow()
         tv_show.name = u'The Walking Dead'
         tv_show.season_number = 4
@@ -111,7 +111,7 @@ class ModelsTestCase(unittest.TestCase):
         tv_show = TVShow.load(tv_show_id=1)
         self.assertEquals(1, len(tv_show))
 
-    def atest_tv_show_with_when(self):
+    def test_tv_show_with_when(self):
         tv_show = TVShow()
         tv_show.name = u'The Walking Dead'
         tv_show.season_number = 4
@@ -127,7 +127,7 @@ class ModelsTestCase(unittest.TestCase):
         tv_show = TVShow.load(tv_show_id=1)
         self.assertEquals(1, len(tv_show))
 
-    def atest_tv_show_with_when_before_air_date(self):
+    def test_tv_show_with_when_before_air_date(self):
         tv_show = TVShow()
         tv_show.name = u'The Walking Dead'
         tv_show.season_number = 4
@@ -142,35 +142,3 @@ class ModelsTestCase(unittest.TestCase):
 
         tv_show = TVShow.load(tv_show_id=1)
         self.assertEquals(1, len(tv_show))
-
-    def test_bittorrent_queue(self):
-        tv_show = TVShow()
-        tv_show.name = u'The Walking Dead'
-        tv_show.season_number = 4
-        tv_show.episode_number = 10
-        tv_show.air_date = datetime.strptime('1989-08-17', '%Y-%m-%d')
-        tv_show.episode_name = u'TWD'
-        tv_show.rating = 9
-        tv_show.create()
-
-        queue = TorrentQueue.load_pending_queue()
-        self.assertEquals(1, len(queue))
-
-        tv_show = TVShow.load(tv_show_id=1)
-        self.assertEquals(1, len(tv_show))
-
-        bittorrent_queue = BittorrentQueue()
-        bittorrent_queue.client_id = 1
-        bittorrent_queue.torrent_id = 2
-        bittorrent_queue.torrent_queue_id = queue[0].torrent_queue_id
-        bittorrent_queue.name = u'The_Walking_Dead_Torrent'
-        bittorrent_queue.url = u'http://blah.The_Walking_Dead_Torrent.torrent'
-        bittorrent_queue.create()
-
-    def _load_sql_schema(slef):
-        path = os.path.dirname(os.path.realpath(__file__))
-        with open(path + '/../model/schema/db_schema.sql', 'r') as f:
-            content = f.read()
-            f.close()
-
-        return content
